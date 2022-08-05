dca95d0231c0648a7ae641da9250ac3eeb56c00a
Markson17 <marksonfrosh@gmail.com>
2022-08-05 02:07:28 +0100

    Secured learning log

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ecoyks#4)6jeanp3yn2+@!s*g#zrm-c8zceca8+sp1+3fb5c^_'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
