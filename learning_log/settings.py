313678e9b445b1c5c94a67e66264b10a2e5d5ae8
Markson Favour <95889759+Markson17@users.noreply.github.com>
2023-02-22 13:08:00 +0000

    Ready to deploy

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'c^qv!stmq+j#j0+%md#@nkh1@k1it&w$px4q!x6n-%-03_51mk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
